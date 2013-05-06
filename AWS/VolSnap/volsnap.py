3cd0b3aaf78dc2dbc5f471854b8b3643c91386bd
Robbie Kershaw <rkershaw@nsml.com.au>
2013-05-06 10:22:00 +1000

    Fix Issues

--- AWS/VolSnap/volsnap.py
+++ AWS/VolSnap/volsnap.py
@@ -1,9 +1,10 @@
 import boto.utils, boto.ec2, time, datetime
 from datetime import timedelta
+from botocreds import aws_access_key_id, aws_secret_access_key
 
 ec2_conn = boto.ec2.connect_to_region("ap-southeast-2", 
-                                      aws_access_key_id="***REMOVED***", 
-                                      aws_secret_access_key="***REMOVED***")
+                                      aws_access_key_id=aws_access_key_id, 
+                                      aws_secret_access_key=aws_secret_access_key)
 
 volumes = ec2_conn.get_all_volumes()
 
@@ -17,9 +18,10 @@ for volume in volumes:
             snap_time =  datetime.datetime.strptime(snap.start_time, boto.utils.ISO8601_MS)
             snap_due = snap_time + timedelta(hours=butime)
             due_now = snap_due < datetime.datetime.utcnow()
-            if due_now:
-                print "Snapping..."
-                volume.create_snapshot('Auto Snapshot Backup')
+        print snaps
+        if due_now:
+            print "Snapping...(Disabled) " + str(volume.id)
+            #volume.create_snapshot('Auto Snapshot Backup')
 
 
             
