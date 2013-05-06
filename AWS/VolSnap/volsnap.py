8f1ade6bd04c835a5a2bb0c334c0349c9253d7c0
Robbie Kershaw <rkershaw@nsml.com.au>
2013-05-06 10:56:15 +1000

    Tidy Logic. Include snap-shotting of previously unsnap-shotted volumes.

--- AWS/VolSnap/volsnap.py
+++ AWS/VolSnap/volsnap.py
@@ -14,19 +14,24 @@ for volume in volumes:
     butime = int(volume.tags.get(tag_id, 0))
     if butime:
         snaps = volume.snapshots()
+        latest_snap_time = None
         for snap in snaps:
             current_snap_time = datetime.datetime.strptime(snap.start_time, boto.utils.ISO8601_MS)
-            try:
+            if latest_snap_time <> None:
                 if current_snap_time > latest_snap_time:
                     latest_snap_time = current_snap_time
-            except(NameError):
+            else:
                 latest_snap_time = current_snap_time
         
-        snap_due = latest_snap_time + timedelta(hours=butime)
-        due_now = snap_due < datetime.datetime.utcnow()
+        if snaps:
+            snap_due = latest_snap_time + timedelta(hours=butime)
+            due_now = snap_due < datetime.datetime.utcnow()
+        else:
+            #Force snapshot if none exists for this volume.
+            due_now = True
         if due_now:
             print "Snapping... " + str(volume.id)
-            volume.create_snapshot('Auto Snapshot Backup')        
+            #volume.create_snapshot('Auto Snapshot Backup')  
             
 
 
