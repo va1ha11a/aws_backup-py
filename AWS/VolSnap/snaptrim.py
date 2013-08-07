7dddefda797a0835a41c3ef29629b3c11fe1bf07
Robbie Kershaw <rkershaw@nsml.com.au>
2013-08-08 09:17:47 +1000

    Added some stuff

---
+++ AWS/VolSnap/snaptrim.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+
+class SnapTrim:
+    def __init__(self):
+        pass
---
+++ AWS/VolSnap/tests/trim_test.py
@@ -0,0 +1,12 @@
+import unittest
+
+
+class DayTestCase(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+    
+    def test_default_list(self):
+        self.assertEqual(1,1)
