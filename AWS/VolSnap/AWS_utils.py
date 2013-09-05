f42bd61a9607b0c9a14a0ad846a0053144a1c5d0
Robbie Kershaw <rkershaw@nsml.com.au>
2013-09-05 16:23:02 +1000

    Created AWS utils to do the heavy lifing on the AWS side. Updated settings to reflect the new design.

--- AWS/VolSnap/AWS_utils.py
+++ AWS/VolSnap/AWS_utils.py
@@ -1,8 +1,39 @@
 #!/usr/bin/env python
 """"Utils that connect to AWS"""
 
-def get_vols_for_backup():
-    pass
+import boto.utils, boto.ec2
+import settings, datetime
 
-def get_snaps_for_vol():
-    pass
+
+class AWSUtils:
+    _ec2_conn = boto.ec2.connect_to_region(settings.target_region, 
+                                          aws_access_key_id=settings.aws_access_key_id, 
+                                          aws_secret_access_key=settings.aws_secret_access_key)
+
+    def _bt_to_dt(self, boto_time):
+        """Convert aws/boto time into a proper python datetime object"""
+        dt = datetime.datetime.strptime(boto_time, boto.utils.ISO8601_MS)
+        return dt
+
+    def get_vols_for_backup(self):
+        """Get a list of all volumes that have the backup enabled tag set to True"""
+        all_vols = self._ec2_conn.get_all_volumes(filters={"tag:"+settings.backup_enabled_tag:True})
+        return [{"id":vol.id, "policy":vol.tags[settings.backup_policy_tag]} for vol in all_vols]
+    
+    def get_snaps_for_vol(self, volume_id):
+        """Get a list of snapshots matching a given volume id and description 
+        and status defined in settings."""
+        snaps = self._ec2_conn.get_all_snapshots(filters={"volume_id":volume_id, 
+                                                          "description":settings.desc, 
+                                                          "status":settings.complete_status})
+        snaps_details = [{"id":snap.id, "start_time":self._bt_to_dt(snap.start_time)} for snap in snaps]
+        return snaps_details
+
+if __name__ == "__main__":
+    x = AWSUtils()
+    vols = x.get_vols_for_backup()
+    y = x.get_snaps_for_vol(vols[0]["id"])
+    print vols
+    print y
+    
+
--- AWS/VolSnap/create_due_snaps.py
+++ AWS/VolSnap/create_due_snaps.py
@@ -1,1 +1,23 @@
 #!/usr/bin/env python
+
+from AWS_utils import AWSUtils
+from policy import defined as defined_policy
+
+class DueSnapshots:
+    
+    def __init__(self):
+        self.AWS = AWSUtils()
+    
+    def _get_vol_list(self):
+        return self.AWS.get_vols_for_backup()
+        
+    def select_due(self):
+        vols = self._get_vol_list()
+        for vol in vols:
+            policy_details = defined_policy[vol["policy"]]
+            print policy_details
+
+
+if __name__ == "__main__":
+    x = DueSnapshots()
+    x.select_due()
