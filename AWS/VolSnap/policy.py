396429fc649717c1f8b33d79acd725cbfb34f06f
Robbie Kershaw <rkershaw@nsml.com.au>
2013-09-05 16:23:38 +1000

    Added a policy infrastructure to handle backup details for later elaboration.

---
+++ AWS/VolSnap/policy.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+""""Backup policy processor. Definitions for backups policies or how to generated 
+them should be stored here"""
+
+defined = {
+           "TEST":{},
+           
+           }
--- AWS/VolSnap/settings.py
+++ AWS/VolSnap/settings.py
@@ -3,3 +3,5 @@ from VolSnap.botocreds import aws_access_key_id, aws_secret_access_key
 target_region = "ap-southeast-2"
 desc = "Auto Snapshot Backup"
 complete_status = "completed"
+backup_enabled_tag = "BU-E"
+backup_policy_tag = "BU-P"
