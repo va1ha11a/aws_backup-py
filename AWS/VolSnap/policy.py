ecd9327b4a1f24daccdbd4ed642bcf995516d5e0
Robbie Kershaw <rkershaw@nsml.com.au>
2013-09-05 16:43:10 +1000

    Definned (hard coded) policy test...for testing...duh

--- AWS/VolSnap/policy.py
+++ AWS/VolSnap/policy.py
@@ -3,6 +3,11 @@
 them should be stored here"""
 
 defined = {
-           "TEST":{},
+           "TEST":{"hourly":(1,24),
+                   "daily":(1,31),
+                   "weekly":(1,12),
+                   "monthly":(1,12),
+                   "yearly":(1,7),
+                   },
            
            }
