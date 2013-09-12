a095f45088411ddddbb5281de731d02a293acf56
Robbie Kershaw <rkershaw@nsml.com.au>
2013-09-12 14:07:22 +1000

    Created first draft of prune_old_snaps.py

---
+++ AWS/VolSnap/prune_old_snaps.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+import logging
+logger = logging.getLogger(__name__)
+
+from AWS_utils import SnapUtils
+
+class PruneSnapshots:
+    
+    def __init__(self):
+        """Set up the AWS object"""
+        self.AWS = SnapUtils()
+           
+    def _choose_snaps(self, snapshots, bu_key):
+        """Filter snapshots by backup key. This allows for checking by key
+        i.e. only could other monthly snapshots when checking for monthly snaps due."""
+        return [snap for snap in snapshots if snap['bu-keys'][bu_key]]
+    
+if __name__ == "__main__":
+    x = PruneSnapshots()
+    print x.AWS.get_snaps_for_vol("vol-bb77fd89")
