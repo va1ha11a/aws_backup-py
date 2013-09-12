8d48eae1cfc28a291b605b2d97b382f5c14c8cb7
Robbie Kershaw <rkershaw@nsml.com.au>
2013-09-12 10:44:49 +1000

    Add basic logging to main to handle output

--- AWS/main.py
+++ AWS/main.py
@@ -1,3 +1,16 @@
 from VolSnap import create_due_snaps
 
-#create_due_snaps.main()
+import logging
+logger = logging.getLogger(__name__)
+logger.addHandler(logging.StreamHandler())
+logger.setLevel(logging.INFO)
+logger.propagate = False
+
+try:
+    result = create_due_snaps.main()    
+except Exception, exc:
+    logger.critical("Exception Occurred:")
+    logger.critical(exc)
+    #TODO: send some kind of alert Possibly email to alert that the whole process is stuffed
+else:
+    logger.info("Created Snapshots: " + str(result))
