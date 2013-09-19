091e285a21b4a0816158ed839a7bed3a13a68890
Robbie Kershaw <rkershaw@nsml.com.au>
2013-09-19 10:45:28 +1000

    Add the run reports launcher script

---
+++ AWS/RunReports.py
@@ -0,0 +1,16 @@
+from VolSnap import email_reports
+
+import logging
+logger = logging.getLogger(__name__)
+logger.addHandler(logging.StreamHandler())
+logger.setLevel(logging.INFO)
+logger.propagate = False
+
+try:
+    result = email_reports.main()    
+except Exception, exc:
+    logger.critical("Exception Occurred:")
+    logger.critical(exc)
+    #TODO: send some kind of alert Possibly email to alert that the whole process is stuffed
+else:
+    logger.info("Report Details: " + str(result))
