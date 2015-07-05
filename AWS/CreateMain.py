c59719f840b5f06f2ed166f28886e08feb178913
Robbie Kershaw <rjkershaw@gmail.com>
2015-07-05 14:37:54 +1000

    testing only

--- AWS/CreateMain.py
+++ AWS/CreateMain.py
@@ -13,6 +13,6 @@ if __name__ == "__main__":
         logger.critical("Exception Occurred:")
         logger.critical(exc)
         #TODO: send some kind of alert 
-        #Possibly email to alert that the whole process is stuffed
+        ##Possibly email to alert that the whole process is stuffed
     else:
         logger.info("Created Snapshots: " + str(result))
